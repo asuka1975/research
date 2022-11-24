47712ea57ad3304560e6dfbad27f09063f065d88
shikina <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2022-11-24 15:23:23 +0900

    fix: task.pyにStochasticCartPoleRealFitnessがimportされていないバグを修正

--- scripts/devneat/task/task.py
+++ scripts/devneat/task/task.py
@@ -1,5 +1,5 @@
 from .space_feed4 import SpaceFeed4
-from .cartpole import CartPole, CartPole1, BrokenPoleCartPole, DoublePoleCartPole, DoublePoleCartPoleGeometricMean, BrokenPoleCartPoleGeometricMean, CartPoleInputSwitching, DoublePoleCartPoleRealFitness, BrokenPoleCartPoleRealFitness
+from .cartpole import CartPole, CartPole1, BrokenPoleCartPole, DoublePoleCartPole, DoublePoleCartPoleGeometricMean, BrokenPoleCartPoleGeometricMean, CartPoleInputSwitching, DoublePoleCartPoleRealFitness, BrokenPoleCartPoleRealFitness, StochasticCartPoleRealFitness
 
 def create_task(task_config):
     return globals()[task_config["name"]](task_config["setting"])
