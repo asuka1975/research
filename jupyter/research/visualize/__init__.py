649adb99d5205695b621ebae206b3924a27f2bf1
shikina <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2022-12-25 16:58:05 +0900

    fix: 前のコミットで消したOpenGL対応のSolverNetworkクラスを__init__でimportしているバグを修正

--- jupyter/research/visualize/__init__.py
+++ jupyter/research/visualize/__init__.py
@@ -2,5 +2,5 @@ from .fitness import Fitness
 from .space_feed import SpaceFeed
 from .neurocomponents import NeuroComponents
 from .devrule import DevRule
-from .solver_network import SolverNetwork, SolverNetworkView
+from .solver_network import SolverNetworkView
 from .cartpole import BrokenPoleCartPole, DoublePoleCartPole, CartPole
